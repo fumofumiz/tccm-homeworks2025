c971c6bda270f87ade86dcbe6ecfdf6d324c4d02
Francesco  Rumiz <frumiz00@login02.g100.cineca.it>
2025-12-22 13:10:12 +0100

    updated main

--- project3/src/md.f90
+++ project3/src/md.f90
@@ -1,46 +1,126 @@
 program main
+         
+        !use md_module
 
         implicit none
          
-        real*8, allocatable :: coord(:,3),mass(:),distance(:,:) !input
-        integer :: Natoms                                       !input
-        integer :: ios
-        real*8, allocatable :: r(:),v(:),a(:) !dynamics vectors
-        character :: debug
-        real*8 :: t,dt !time and time step
+        real*8, allocatable :: coord(:,3),mass(:),distance(:,:) ! Input coordinates, masses 
+        integer :: Natoms                                       ! Input number of atoms
+        character(len=200) :: input_file                        ! Input file
+        real*8 :: eps,sigma                                     ! Lennard-Jones parameters
+        integer :: ios ! Input output error 
+        real*8, allocatable :: velocity(:,3),acc(:,3) ! Dynamics arrays
+        character :: debug ! Debug option
+        real*8 :: dt ! Dynamics parameters: time step
+        integer :: nsteps ! Dynamics parameters: number of steps
+        real*8 :: Kinetic,Potential,Total ! Energies
 
+        !---- Parameters ----
 
+        ! 1 Angstrom = 0.1 nm
+
+        ! Lennard-Jones potential parameters
+
+        eps = 0.997 ! kJ/mol
+        sigma = 0.3405 ! In nanometers, 3.405 Angstrom 
+
+        ! Debug option
+        
+        debug='y'
+          
+        ! Input dynamics parameters
+        dt = 0.02                        ! In picoseconds 
+        nsteps = 1000
+                
+        !---- Read input and compute distances ----
+
+        input_file = 'inp.txt'
+        Natoms = read_Natoms(input_file)
+        allocate(coord(Natoms,3),mass(Natoms),distance(Natoms,Natoms))
+        call read_molecule(input_file,Natoms,coord,mass)
+        distance = compute_distances(Natoms,coord,distance)
+
+        ! Checking that the input is correctly read and the distances are correct 
+        if (debug.eq.'y') then
+         
+         write(*,*) '---- Input checking ----'
+         write(*,*) 'Number of atoms', Natoms
+         write(*,*) 
+         write(*,*) 'Coordinates and mass'
+         do i=1,Natoms
+                 write(*,*) coord(i,:), mass(i)
+         enddo
+         write(*,*) 
+         write(*,*) 'Distance matrix'
+         write(*,*)
+         do i=1,Natoms
+                 write(*,*) d(i,:)
+         enddo
+        
+        endif 
+
+        ! Initialize velocity and compute energies and accelerations
+        allocate(velocity(Natoms,3),acc(Natoms,3))
+        v = 0.d0
+        Kinetic = T(Natoms,velocity,mass)
+        Potential = V(eps,sigma,Natoms,distance)
+        Total = Kinetic + Potential
+        call compute_acc(Natoms,coord,mass,distance,acc)
+
+        if (debug.eq.'y') then
+
+         write(*,*) '---- Functions checking ----'
+         write(*,*)
+         write(*,*) 'Kinetic energy:', Kinetic, 'Potential energy:', Potential, 'Total energy:', Total
+         write(*,*) 
+         write(*,*) 'Accelerations'
+         do i=1,Natoms
+                write(*,*) acc(i,:)
+         enddo
+         write(*,*)
+
+        endif 
+
+        ! Writing output file
+        open(1,file='out.txt',iostat=ios) 
+        
+        ! Checking for errors
+        if (ios.ne.0) then
+                write(*,*) 'Error in writing the file'
+        endif
+
+        ! Writing zero step
+        write(1,*) Natoms
+        write(1,*) 'Kinetic energy:', Kinetic, 'Potential energy:', Potential, 'Total energy:', Total, 'Time (ps):', 0.d0
+        do i=1,Natoms
+         write(1,*) 'Ar', coord(i,:)*10.d0 ! Coordinates in angstrom
+        enddo 
 
         !---- Verlet algorithm ----
 
-        allocate(v(Natoms))
-
-        ! Input parameters
-        t=0.d0
-        dt=0.02
-        nsteps=1000
-
-        !Initialize velocity
-        v=0.d0
-
-        !Algorithm
-
-        open(1,file='output',iostat=ios) 
-
         do i=1,nsteps
 
-         r = r + v*t + a*((dt)**2)*0.5d0
-         v = v + 0.5d0*a*dt
-         call compute_acc(Natoms,coord,mass,distance,a)
-         v = v + 0.5d0*a*dt
+         coord = coord + v*dt + acc*((dt)**2)*0.5d0        ! Update positions
+         v = v + 0.5d0*acc*dt                              ! Update velocities step 1
+         call compute_acc(Natoms,coord,mass,distance,acc)  ! Update accelerations
+         v = v + 0.5d0*acc*dt                              ! Update velocities step 2
 
-         if (nsteps%10.eq.0) then
+         ! Now we that we have the new positions and velocities we compute new distances, kinetic, potential and total energy. 
+         distance = compute_distances(Natoms,coord,distance)       
+         Kinetic=T(Natoms,velocity,mass)
+         Potential=V(eps,sigma,Natoms,distance)
+         Total=Kinetic + Potential
 
-                write(*,*) 'Kinetic energy:', K, 'Potential energy:', V, 'Total energy:', E, 'Time:', t,'Conservation:' !TODO: add conservation
-                
+         ! Write new coordinates on the output file every 10 steps
+         if (mod(i,10).eq.0) then
+                write(1,*) Natoms
+                write(1,*) 'Kinetic energy:', Kinetic, 'Potential energy:', Potential, 'Total energy:', Total, 'Time:', dt*i,'Conservation:' !TODO: add conservation
+                do i=1,Natoms
+                 write(1,*) 'Ar', coord(i,:)*10.d0 ! Coordinates in angstrom
+                enddo
 
          endif
-         
+
         enddo
 
         close(1)
